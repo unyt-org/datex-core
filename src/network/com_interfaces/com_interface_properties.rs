72278027ffa9bef7e70cb54c481a531b099b8b4d
Jonas Strehle <jonas@strehles.info>
2025-04-28 19:15:05 +0200

    Work on reconnection logic for com interfaces (WIP)

--- src/network/com_interfaces/com_interface_properties.rs
+++ src/network/com_interfaces/com_interface_properties.rs
@@ -12,25 +12,22 @@ pub enum InterfaceDirection {
 pub struct InterfaceProperties {
     /// the type of the interface, by which it is identified
     /// e.g. "tcp-client", "websocket-server",
-    /// multiple interfaces implementations (e.g. for native and web) 
-    /// can have the same interface type if they are compatible and 
+    /// multiple interfaces implementations (e.g. for native and web)
+    /// can have the same interface type if they are compatible and
     /// have an identical initialization function
     pub interface_type: String,
-    
-    /// the channel that the interface is using, 
+
+    /// the channel that the interface is using,
     /// e.g. "tcp", "websocket"
     pub channel: String,
-    
+
     /// a unique name that further identifies an interface instance
     /// e.g. "wss://example.com:443"
     pub name: Option<String>,
-    
+
     /// The support message direction of the interface
     pub direction: InterfaceDirection,
 
-    /// Time in milliseconds to wait before reconnecting after a connection error
-    pub reconnect_interval: Option<Duration>,
-    
     /// Estimated mean latency for this interface type in milliseconds (round trip time).
     /// Lower latency interfaces are preferred over higher latency channels
     pub round_trip_time: Duration,
@@ -40,16 +37,33 @@ pub struct InterfaceProperties {
 
     /// If true, the interface does support continuous connections
     pub continuous_connection: bool,
-    
+
     /// If true, the interface can be used to redirect DATEX messages to other endpoints
     /// which are not directly connected to the interface (default: true)
     /// Currently only enforced for broadcast messages
     pub allow_redirects: bool,
-    
+
     /// If true, the interface is a secure channel (can not be eavesdropped).
     /// This might be an already encrypted channel such as WebRTC or a channel
     /// that is end-to-end and not interceptable by third parties
     pub is_secure_channel: bool,
+
+    // Defines the reconnection strategy for the interface
+    // If the interface is not able to reconnect, it will be destroyed
+    pub reconnection_config: ReconnectionConfig,
+}
+
+#[derive(Debug, Clone)]
+pub enum ReconnectionConfig {
+    NoReconnect,
+    InstantReconnect,
+    ReconnectWithTimeout { timeout: Duration },
+    ReconnectWithTimeoutAndAttempts { timeout: Duration, attempts: u8 },
+}
+impl Default for ReconnectionConfig {
+    fn default() -> Self {
+        ReconnectionConfig::NoReconnect
+    }
 }
 
 impl InterfaceProperties {
@@ -58,6 +72,13 @@ impl InterfaceProperties {
             InterfaceDirection::In => false,
             InterfaceDirection::Out => true,
             InterfaceDirection::InOut => true,
+        }
+    }
+
+    pub fn shall_reconnect(&self) -> bool {
+        match self.reconnection_config {
+            ReconnectionConfig::NoReconnect => false,
+            _ => true,
         }
     }
 
@@ -77,12 +98,12 @@ impl Default for InterfaceProperties {
             channel: "".to_string(),
             name: None,
             direction: InterfaceDirection::InOut,
-            reconnect_interval: None,
             round_trip_time: Duration::from_millis(0),
             max_bandwidth: u32::MAX,
             continuous_connection: false,
             allow_redirects: true,
             is_secure_channel: false,
+            reconnection_config: ReconnectionConfig::default(),
         }
     }
 }
