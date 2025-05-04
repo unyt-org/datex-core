399f5b8d4b96ec23425f300928e942750b60a296
Jonas Strehle <jonas@strehles.info>
2025-05-04 10:48:25 +0200

    Work on webrtc (WIP)

--- src/network/com_interfaces/default_com_interfaces/webrtc/webrtc_new_client_interface.rs
+++ src/network/com_interfaces/default_com_interfaces/webrtc/webrtc_new_client_interface.rs
@@ -13,7 +13,10 @@ use log::{debug, info};
 use webrtc::{
     api::{media_engine::MediaEngine, APIBuilder},
     data_channel::{data_channel_message::DataChannelMessage, RTCDataChannel},
-    ice_transport::ice_candidate::RTCIceCandidate,
+    ice_transport::{
+        ice_candidate::{RTCIceCandidate, RTCIceCandidateInit},
+        ice_gatherer::OnLocalCandidateHdlrFn,
+    },
     peer_connection::{
         configuration::RTCConfiguration,
         peer_connection_state::RTCPeerConnectionState,
@@ -21,6 +24,7 @@ use webrtc::{
     },
 };
 
+use crate::network::com_interfaces::com_interface::ComInterfaceState;
 use crate::{
     delegate_com_interface_info,
     network::com_interfaces::{
@@ -34,15 +38,12 @@ use crate::{
     },
     set_opener,
 };
-use crate::{
-    network::com_interfaces::com_interface::ComInterfaceState, task::spawn,
-};
 
 use super::webrtc_common::WebRTCError;
 
 pub struct WebRTCNewClientInterface {
     info: ComInterfaceInfo,
-    peer_connections: HashMap<String, Arc<RTCPeerConnection>>,
+    peer_connection: Option<Arc<RTCPeerConnection>>,
 }
 impl MultipleSocketProvider for WebRTCNewClientInterface {
     fn provide_sockets(&self) -> Arc<Mutex<ComInterfaceSockets>> {
@@ -56,152 +57,205 @@ impl WebRTCNewClientInterface {
         let info = ComInterfaceInfo::new();
         WebRTCNewClientInterface {
             info,
-            peer_connections: HashMap::new(),
+            peer_connection: None,
         }
     }
 
     #[create_opener]
     async fn open(&mut self) -> Result<(), WebRTCError> {
+        let api = APIBuilder::new().build();
+        let peer_connection = Arc::new(
+            api.new_peer_connection(Default::default()).await.unwrap(),
+        );
+        self.peer_connection = Some(peer_connection.clone());
         Ok(())
     }
 
-    pub async fn set_offer(
-        &mut self,
-        id: &str,
-        offer: RTCSessionDescription,
-    ) -> Result<(), WebRTCError> {
-        let peer_connection = self
-            .peer_connections
-            .get(id)
-            .ok_or(WebRTCError::InvalidURL)?;
-
-        peer_connection.set_remote_description(offer).await.unwrap();
-        // let answer = peer_connection.create_answer(None).await.unwrap();
-
-        // let mut gather_complete =
-        //     peer_connection.gathering_complete_promise().await;
-
-        // peer_connection.set_local_description(answer).await.unwrap();
-        // let _ = gather_complete.recv().await;
-        // let local_desc = peer_connection.local_description().await.unwrap();
-        // info!("Local description: {:?}", local_desc);
-        Ok(())
-    }
-
-    pub async fn create_offer(&mut self, id: &str) -> RTCSessionDescription {
-        let mut media_engine = MediaEngine::default();
-        media_engine.register_default_codecs().unwrap();
-
-        let api = APIBuilder::new() /*.with_media_engine(media_engine) */
-            .build();
-        let config = RTCConfiguration::default(); // FIXME allow custom config
-        let peer_connection =
-            Arc::new(api.new_peer_connection(config).await.unwrap());
-        let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
-
-        self.setup_ice_candidates(peer_connection.clone()).await;
-        // self.setup_tracks(peer_connection.clone()).await;
-        peer_connection.on_peer_connection_state_change(Box::new(
-            move |s: RTCPeerConnectionState| {
-                info!("Peer connection state changed: {:?}", s);
-                match s {
-                    RTCPeerConnectionState::Connected => {
-                        info!("Peer connection is connected");
-
-                        let _ = done_tx.try_send(());
-                    }
-                    RTCPeerConnectionState::Disconnected => {
-                        info!("Peer connection is disconnected");
-
-                        let _ = done_tx.try_send(());
-                    }
-                    RTCPeerConnectionState::Failed => {
-                        info!("Peer connection failed");
-                        let _ = done_tx.try_send(());
-                    }
-                    _ => {}
-                }
-                Box::pin(async {})
-            },
-        ));
-
-        peer_connection
-            .on_data_channel(Box::new(move |channel: Arc<RTCDataChannel>| {
-                let channel_label = channel.label().to_owned();
-                let channel_id = channel.id();
-                println!("New DataChannel {} {}", channel_label, channel_id);
-
-                // Register channel opening handling
-                Box::pin(async move {
-                    let channel_clone = Arc::clone(&channel);
-                    let channel_label_clone = channel_label.clone();
-                    let channel_id = channel_id.clone();
-                    channel.on_open(Box::new(move || {
-                        info!("Data channel '{}'-'{}' open.", channel_label_clone, channel_id);
-                        Box::pin(async move {
-                            let mut result = Result::<usize, webrtc::Error>::Ok(0);
-                            while result.is_ok() {
-                                let timeout = tokio::time::sleep(Duration::from_secs(5));
-                                tokio::pin!(timeout);
-
-                                tokio::select! {
-                                    _ = timeout.as_mut() =>{
-                                        result = channel_clone.send_text("hello").await.map_err(Into::into);
-                                    }
-                                };
-                            }
-                        })
-                    }));
-
-                    // Register text message handling
-                    channel.on_message(Box::new(move |msg: DataChannelMessage| {
-                        let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
-                        println!("Message from DataChannel '{}': '{}'", channel_label, msg_str);
-                        Box::pin(async {})
-                    }));
-                })
-            }));
-        self.peer_connections
-            .insert(id.to_string(), peer_connection.clone());
-        let mut offer = peer_connection.create_offer(None).await.unwrap();
-        spawn(async move {
-            done_rx.recv().await;
-        });
-
-        let mut gather_complete =
-            peer_connection.gathering_complete_promise().await;
-
-        peer_connection.set_local_description(offer).await.unwrap();
-        let _ = gather_complete.recv().await;
-
-        if let Some(local_desc) = peer_connection.local_description().await {
-            info!("Local description: {:?}", local_desc);
-            return local_desc;
+    pub async fn create_offer(&self) -> RTCSessionDescription {
+        if let Some(peer_connection) = &self.peer_connection {
+            let offer = peer_connection.create_offer(None).await.unwrap();
+            peer_connection
+                .set_local_description(offer.clone())
+                .await
+                .unwrap();
+            offer
         } else {
-            panic!("Failed to get local description");
+            panic!("Peer connection not initialized");
         }
     }
 
-    async fn setup_ice_candidates(
-        &self,
-        peer_connection: Arc<RTCPeerConnection>,
-    ) {
-        peer_connection.on_ice_candidate(Box::new(
-            |candidate: Option<RTCIceCandidate>| {
-                if let Some(candidate) = candidate {
-                    info!("New ICE candidate: {:?}", candidate);
-                    // Send the candidate to the remote peer through signaling server
-                }
-                Box::pin(async {})
-            },
-        ));
+    pub async fn set_remote_description(&self, sdp: RTCSessionDescription) {
+        if let Some(peer_connection) = &self.peer_connection {
+            peer_connection.set_remote_description(sdp).await.unwrap();
+        } else {
+            panic!("Peer connection not initialized");
+        }
     }
-    async fn setup_tracks(&self, peer_connection: Arc<RTCPeerConnection>) {
-        peer_connection.on_track(Box::new(|track, _, _| {
-            info!("New track received: {:?}", track);
-            Box::pin(async {})
-        }));
+
+    pub async fn create_answer(&self) -> RTCSessionDescription {
+        if let Some(peer_connection) = &self.peer_connection {
+            let answer = peer_connection.create_answer(None).await.unwrap();
+            peer_connection
+                .set_local_description(answer.clone())
+                .await
+                .unwrap();
+            answer
+        } else {
+            panic!("Peer connection not initialized");
+        }
     }
+
+    pub async fn add_ice_candidate(&self, candidate: RTCIceCandidateInit) {
+        if let Some(peer_connection) = &self.peer_connection {
+            peer_connection.add_ice_candidate(candidate).await.unwrap();
+        }
+    }
+
+    pub fn on_ice_candidate(&self, f: OnLocalCandidateHdlrFn) {
+        if let Some(peer_connection) = &self.peer_connection {
+            peer_connection.on_ice_candidate(f);
+        } else {
+            panic!("Peer connection not initialized");
+        }
+    }
+
+    // pub async fn set_offer(
+    //     &mut self,
+    //     id: &str,
+    //     offer: RTCSessionDescription,
+    // ) -> Result<(), WebRTCError> {
+    //     let peer_connection = self
+    //         .peer_connections
+    //         .get(id)
+    //         .ok_or(WebRTCError::InvalidURL)?;
+
+    //     peer_connection.set_remote_description(offer).await.unwrap();
+    //     // let answer = peer_connection.create_answer(None).await.unwrap();
+
+    //     // let mut gather_complete =
+    //     //     peer_connection.gathering_complete_promise().await;
+
+    //     // peer_connection.set_local_description(answer).await.unwrap();
+    //     // let _ = gather_complete.recv().await;
+    //     // let local_desc = peer_connection.local_description().await.unwrap();
+    //     // info!("Local description: {:?}", local_desc);
+    //     Ok(())
+    // }
+
+    // pub async fn create_offer(&mut self, id: &str) -> RTCSessionDescription {
+    //     let mut media_engine = MediaEngine::default();
+    //     media_engine.register_default_codecs().unwrap();
+
+    //     let api = APIBuilder::new() /*.with_media_engine(media_engine) */
+    //         .build();
+    //     let config = RTCConfiguration::default(); // FIXME allow custom config
+    //     let peer_connection =
+    //         Arc::new(api.new_peer_connection(config).await.unwrap());
+    //     let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
+
+    //     self.setup_ice_candidates(peer_connection.clone()).await;
+    //     // self.setup_tracks(peer_connection.clone()).await;
+    //     peer_connection.on_peer_connection_state_change(Box::new(
+    //         move |s: RTCPeerConnectionState| {
+    //             info!("Peer connection state changed: {:?}", s);
+    //             match s {
+    //                 RTCPeerConnectionState::Connected => {
+    //                     info!("Peer connection is connected");
+
+    //                     let _ = done_tx.try_send(());
+    //                 }
+    //                 RTCPeerConnectionState::Disconnected => {
+    //                     info!("Peer connection is disconnected");
+
+    //                     let _ = done_tx.try_send(());
+    //                 }
+    //                 RTCPeerConnectionState::Failed => {
+    //                     info!("Peer connection failed");
+    //                     let _ = done_tx.try_send(());
+    //                 }
+    //                 _ => {}
+    //             }
+    //             Box::pin(async {})
+    //         },
+    //     ));
+
+    //     peer_connection
+    //         .on_data_channel(Box::new(move |channel: Arc<RTCDataChannel>| {
+    //             let channel_label = channel.label().to_owned();
+    //             let channel_id = channel.id();
+    //             println!("New DataChannel {} {}", channel_label, channel_id);
+
+    //             // Register channel opening handling
+    //             Box::pin(async move {
+    //                 let channel_clone = Arc::clone(&channel);
+    //                 let channel_label_clone = channel_label.clone();
+    //                 let channel_id = channel_id.clone();
+    //                 channel.on_open(Box::new(move || {
+    //                     info!("Data channel '{}'-'{}' open.", channel_label_clone, channel_id);
+    //                     Box::pin(async move {
+    //                         let mut result = Result::<usize, webrtc::Error>::Ok(0);
+    //                         while result.is_ok() {
+    //                             let timeout = tokio::time::sleep(Duration::from_secs(5));
+    //                             tokio::pin!(timeout);
+
+    //                             tokio::select! {
+    //                                 _ = timeout.as_mut() =>{
+    //                                     result = channel_clone.send_text("hello").await.map_err(Into::into);
+    //                                 }
+    //                             };
+    //                         }
+    //                     })
+    //                 }));
+
+    //                 // Register text message handling
+    //                 channel.on_message(Box::new(move |msg: DataChannelMessage| {
+    //                     let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
+    //                     println!("Message from DataChannel '{}': '{}'", channel_label, msg_str);
+    //                     Box::pin(async {})
+    //                 }));
+    //             })
+    //         }));
+    //     self.peer_connections
+    //         .insert(id.to_string(), peer_connection.clone());
+    //     let mut offer = peer_connection.create_offer(None).await.unwrap();
+    //     spawn(async move {
+    //         done_rx.recv().await;
+    //     });
+
+    //     let mut gather_complete =
+    //         peer_connection.gathering_complete_promise().await;
+
+    //     peer_connection.set_local_description(offer).await.unwrap();
+    //     let _ = gather_complete.recv().await;
+
+    //     if let Some(local_desc) = peer_connection.local_description().await {
+    //         info!("Local description: {:?}", local_desc);
+    //         return local_desc;
+    //     } else {
+    //         panic!("Failed to get local description");
+    //     }
+    // }
+
+    // async fn setup_ice_candidates(
+    //     &self,
+    //     peer_connection: Arc<RTCPeerConnection>,
+    // ) {
+    //     peer_connection.on_ice_candidate(Box::new(
+    //         |candidate: Option<RTCIceCandidate>| {
+    //             if let Some(candidate) = candidate {
+    //                 info!("New ICE candidate: {:?}", candidate);
+    //                 // Send the candidate to the remote peer through signaling server
+    //             }
+    //             Box::pin(async {})
+    //         },
+    //     ));
+    // }
+    // async fn setup_tracks(&self, peer_connection: Arc<RTCPeerConnection>) {
+    //     peer_connection.on_track(Box::new(|track, _, _| {
+    //         info!("New track received: {:?}", track);
+    //         Box::pin(async {})
+    //     }));
+    // }
 }
 
 impl ComInterface for WebRTCNewClientInterface {
--- tests/network/com_interfaces/webrtc_2.rs
+++ tests/network/com_interfaces/webrtc_2.rs
@@ -1,7 +1,7 @@
 use datex_core::network::com_interfaces::default_com_interfaces::webrtc::webrtc_new_client_interface::WebRTCNewClientInterface;
 use log::info;
 use ntest_timeout::timeout;
-use webrtc::media::audio::buffer::info;
+use webrtc::{ice_transport::ice_candidate::RTCIceCandidate, media::audio::buffer::info};
 
 use crate::context::init_global_context;
 
@@ -15,20 +15,36 @@ pub async fn test_send_receive() {
     let mut interface_b = WebRTCNewClientInterface::new("b");
     interface_b.open().await.unwrap();
 
-    let session_request_a_to_b = interface_a.create_offer("@b").await;
-    info!("Session request: {:?}", session_request_a_to_b);
+    // interface_a.on_ice_candidate(Box::new(
+    //     |candidate: Option<RTCIceCandidate>| {
+    //         if let Some(candidate) = candidate {
+    //             info!("New ICE candidate: {:?}", candidate);
+    //         }
+    //         Box::pin(async {
+    //             interface_b.add_ice_candidate(candidate).await.unwrap();
+    //         })
+    //     },
+    // ));
 
-    let session_request_b_to_a = interface_b.create_offer("@a").await;
-    info!("Session request: {:?}", session_request_b_to_a);
+    let offer = interface_a.create_offer().await;
+    interface_b.set_remote_description(offer).await;
 
-    interface_a
-        .set_offer("@b", session_request_b_to_a)
-        .await
-        .unwrap();
+    let answer = interface_b.create_answer().await;
+    interface_a.set_remote_description(answer).await;
+    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+
+    // let session_request_a_to_b = interface_a.create_offer("@b").await;
+    // info!("Session request: {:?}", session_request_a_to_b);
+
+    // let session_request_b_to_a = interface_b.create_offer("@a").await;
+    // info!("Session request: {:?}", session_request_b_to_a);
+
+    // interface_a
+    //     .set_offer("@b", session_request_b_to_a)
+    //     .await
+    //     .unwrap();
     // interface_b
     //     .set_offer("@a", session_request_a_to_b)
     //     .await
     //     .unwrap();
-
-    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
 }
