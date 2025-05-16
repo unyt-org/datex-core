7278528d6c7f375ec60cdc4c0937c263bac71a8e
Jonas Strehle <jonas@strehles.info>
2025-05-16 19:56:57 +0200

    Big refactoring webrtc (WIP)

--- src/network/com_interfaces/default_com_interfaces/webrtc/webrtc_common_new/webrtc_trait.rs
+++ src/network/com_interfaces/default_com_interfaces/webrtc/webrtc_common_new/webrtc_trait.rs
@@ -28,16 +28,7 @@ use super::{
 };
 
 #[async_trait(?Send)]
-pub trait PubWebRTCTrait<T: 'static> {
-    fn new(peer_endpoint: impl Into<Endpoint>) -> Self;
-    fn new_with_ice_servers(
-        peer_endpoint: impl Into<Endpoint>,
-        ice_servers: Vec<RTCIceServer>,
-    ) -> Self;
-}
-
-#[async_trait(?Send)]
-pub trait WebRTCTrait<T: 'static> {
+pub trait WebRTCTraitInternal<T: 'static> {
     // These method must be implemented in the interface
     fn provide_data_channels(&self) -> Rc<RefCell<DataChannels<T>>>;
     fn get_commons(&self) -> Rc<RefCell<WebRTCCommon>>;
@@ -68,44 +59,6 @@ pub trait WebRTCTrait<T: 'static> {
         &self,
     ) -> Result<RTCSessionDescriptionDX, WebRTCError>;
 
-    // This must be called in the open method
-    fn setup_listeners(&self) {
-        let data_channels = self.provide_data_channels();
-        let data_channels_clone = data_channels.clone();
-
-        let info = self.provide_info();
-        let interface_uuid = info.get_uuid().clone();
-        let sockets = info.com_interface_sockets();
-
-        let remote_endpoint = self.remote_endpoint();
-        data_channels.borrow_mut().on_add =
-            Some(Box::new(move |data_channel| {
-                let data_channel = data_channel.clone();
-                let data_channels_clone = data_channels_clone.clone();
-                let sockets = sockets.clone();
-                let interface_uuid = interface_uuid.clone();
-                let remote_endpoint = remote_endpoint.clone();
-                Box::pin(async move {
-                    Self::setup_data_channel(
-                        remote_endpoint.clone(),
-                        interface_uuid.clone(),
-                        sockets.clone(),
-                        data_channels_clone.clone(),
-                        data_channel,
-                    )
-                    .await
-                    .unwrap()
-                })
-            }));
-    }
-    fn set_ice_servers(&self, ice_servers: Vec<RTCIceServer>) {
-        let commons = self.get_commons();
-        let mut commons = commons.borrow_mut();
-        commons.ice_servers = ice_servers;
-    }
-    fn remote_endpoint(&self) -> Endpoint {
-        self.get_commons().borrow().endpoint.clone()
-    }
     fn set_on_ice_candidate(&self, on_ice_candidate: Box<dyn Fn(Vec<u8>)>) {
         self.get_commons().borrow_mut().on_ice_candidate =
             Some(on_ice_candidate);
@@ -148,37 +101,8 @@ pub trait WebRTCTrait<T: 'static> {
             .expect("Failed to register socket endpoint");
         socket_uuid
     }
-    async fn create_offer(&self) -> Result<Vec<u8>, WebRTCError> {
-        let data_channel = self.handle_create_data_channel().await?;
-        let data_channel_rc = Rc::new(RefCell::new(data_channel));
-        let data_channels = self.provide_data_channels();
-        {
-            let info = self.provide_info();
-            let interface_uuid = info.get_uuid().clone();
-            let sockets = info.com_interface_sockets();
-            Self::setup_data_channel(
-                self.remote_endpoint(),
-                interface_uuid,
-                sockets.clone(),
-                data_channels,
-                data_channel_rc.clone(),
-            )
-            .await?;
-        }
-        let offer = self.handle_create_offer().await?;
-        self.handle_set_local_description(offer.clone()).await?;
-        let offer = serialize(&offer).unwrap();
-        Ok(offer)
-    }
-    async fn create_answer(
-        &self,
-        offer: Vec<u8>,
-    ) -> Result<Vec<u8>, WebRTCError> {
-        self.set_remote_description(offer).await?;
-        let answer = self.handle_create_answer().await?;
-        self.handle_set_local_description(answer.clone()).await?;
-        let answer = serialize(&answer).unwrap();
-        Ok(answer)
+    fn _remote_endpoint(&self) -> Endpoint {
+        self.get_commons().borrow().endpoint.clone()
     }
     async fn set_remote_description(
         &self,
@@ -249,7 +173,87 @@ pub trait WebRTCTrait<T: 'static> {
         Self::handle_setup_data_channel(channel).await?;
         Ok(())
     }
+}
+
+#[async_trait(?Send)]
+pub trait WebRTCTrait<T: 'static>: WebRTCTraitInternal<T> {
+    fn new(peer_endpoint: impl Into<Endpoint>) -> Self;
+    fn new_with_ice_servers(
+        peer_endpoint: impl Into<Endpoint>,
+        ice_servers: Vec<RTCIceServer>,
+    ) -> Self;
+    async fn create_offer(&self) -> Result<Vec<u8>, WebRTCError> {
+        let data_channel = self.handle_create_data_channel().await?;
+        let data_channel_rc = Rc::new(RefCell::new(data_channel));
+        let data_channels = self.provide_data_channels();
+        {
+            let info = self.provide_info();
+            let interface_uuid = info.get_uuid().clone();
+            let sockets = info.com_interface_sockets();
+            Self::setup_data_channel(
+                self._remote_endpoint(),
+                interface_uuid,
+                sockets.clone(),
+                data_channels,
+                data_channel_rc.clone(),
+            )
+            .await?;
+        }
+        let offer = self.handle_create_offer().await?;
+        self.handle_set_local_description(offer.clone()).await?;
+        let offer = serialize(&offer).unwrap();
+        Ok(offer)
+    }
+    async fn create_answer(
+        &self,
+        offer: Vec<u8>,
+    ) -> Result<Vec<u8>, WebRTCError> {
+        self.set_remote_description(offer).await?;
+        let answer = self.handle_create_answer().await?;
+        self.handle_set_local_description(answer.clone()).await?;
+        let answer = serialize(&answer).unwrap();
+        Ok(answer)
+    }
+
     async fn set_answer(&self, answer: Vec<u8>) -> Result<(), WebRTCError> {
         self.set_remote_description(answer).await
+    }
+    // This must be called in the open method
+    fn setup_listeners(&self) {
+        let data_channels = self.provide_data_channels();
+        let data_channels_clone = data_channels.clone();
+
+        let info = self.provide_info();
+        let interface_uuid = info.get_uuid().clone();
+        let sockets = info.com_interface_sockets();
+
+        let remote_endpoint = self.remote_endpoint();
+        data_channels.borrow_mut().on_add =
+            Some(Box::new(move |data_channel| {
+                let data_channel = data_channel.clone();
+                let data_channels_clone = data_channels_clone.clone();
+                let sockets = sockets.clone();
+                let interface_uuid = interface_uuid.clone();
+                let remote_endpoint = remote_endpoint.clone();
+                Box::pin(async move {
+                    Self::setup_data_channel(
+                        remote_endpoint.clone(),
+                        interface_uuid.clone(),
+                        sockets.clone(),
+                        data_channels_clone.clone(),
+                        data_channel,
+                    )
+                    .await
+                    .unwrap()
+                })
+            }));
+    }
+    fn set_ice_servers(&self, ice_servers: Vec<RTCIceServer>) {
+        let commons = self.get_commons();
+        let mut commons = commons.borrow_mut();
+        commons.ice_servers = ice_servers;
+    }
+    fn remote_endpoint(&self) -> Endpoint {
+        self._remote_endpoint()
     }
 }
