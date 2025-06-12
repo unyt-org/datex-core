97cb2ed544b484ce92f1d611731cc6804cc465f7
Benedikt Strehle <benedikt@unyt.org>
2025-06-13 00:15:21 +0200

    rename benchmarks

--- benches/mod.rs
+++ benches/mod.rs
@@ -20,25 +20,25 @@ fn bench_json_file(c: &mut Criterion, file_path: &str) {
     let json = get_json_test_string(file_path);
     let dxb  = compile_script(&json).expect("Failed to parse JSON string");
     // serde
-    c.bench_with_input(BenchmarkId::new("json to runtime value", file_path), &json, |b, json| {
+    c.bench_with_input(BenchmarkId::new("json to runtime value serde_json", file_path), &json, |b, json| {
         b.iter(|| {
             black_box(json_to_runtime_value_baseline_serde(black_box(json)));
         })
     });
     // json_syntax
-    c.bench_with_input(BenchmarkId::new("json_syntax to runtime value", file_path), &json, |b, json| {
+    c.bench_with_input(BenchmarkId::new("json to runtime value json_syntax", file_path), &json, |b, json| {
         b.iter(|| {
             black_box(json::json_to_runtime_value_baseline_json_syntax(black_box(json)));
         })
     });
     // DATEX
-    c.bench_with_input(BenchmarkId::new("datex to runtime value", file_path), &json, |b, json| {
+    c.bench_with_input(BenchmarkId::new("json to runtime value datex", file_path), &json, |b, json| {
         b.iter(|| {
             black_box(json::json_to_runtime_value_datex(black_box(json)));
         })
     });
     // JSON string to DXB
-    c.bench_with_input(BenchmarkId::new("json to DXB", file_path), &json, |b, json| {
+    c.bench_with_input(BenchmarkId::new("json to dxb", file_path), &json, |b, json| {
         b.iter(|| {
             black_box(json_to_dxb(black_box(json)));
         })
@@ -56,31 +56,31 @@ fn bench_json_file(c: &mut Criterion, file_path: &str) {
     let json_syntax = json::json_to_json_syntax_value(&json);
     let json_datex = json::json_to_datex_value(&json);
     // serde
-    c.bench_with_input(BenchmarkId::new("runtime value to serde JSON", file_path), &json_serde, |b, json_serde| {
+    c.bench_with_input(BenchmarkId::new("runtime value to json serde_json", file_path), &json_serde, |b, json_serde| {
         b.iter(|| {
             black_box(json::runtime_value_to_json_baseline_serde_json(black_box(json_serde)));
         })
     });
     // json_syntax
-    c.bench_with_input(BenchmarkId::new("runtime value to json_syntax JSON", file_path), &json_syntax, |b, json_syntax| {
+    c.bench_with_input(BenchmarkId::new("runtime value to json json_syntax", file_path), &json_syntax, |b, json_syntax| {
         b.iter(|| {
             black_box(json::runtime_value_to_json_baseline_json_syntax(black_box(json_syntax)));
         })
     });
     // DATEX
-    c.bench_with_input(BenchmarkId::new("runtime value to DATEX JSON", file_path), &json_datex, |b, json_datex| {
+    c.bench_with_input(BenchmarkId::new("runtime value to json datex", file_path), &json_datex, |b, json_datex| {
         b.iter(|| {
             black_box(json::runtime_value_to_json_datex(black_box(json_datex)));
         })
     });
     // DXB
-    c.bench_with_input(BenchmarkId::new("runtime value to DXB", file_path), &json_datex, |b, json_datex| {
+    c.bench_with_input(BenchmarkId::new("runtime value to dxb", file_path), &json_datex, |b, json_datex| {
         b.iter(|| {
             black_box(json::runtime_value_to_dxb(black_box(json_datex)));
         })
     });
     // DXB to JSON
-    c.bench_with_input(BenchmarkId::new("DXB to JSON", file_path), &dxb, |b, dxb| {
+    c.bench_with_input(BenchmarkId::new("dxb to json", file_path), &dxb, |b, dxb| {
         b.iter(|| {
             black_box(json::dxb_to_json(black_box(dxb)));
         })
