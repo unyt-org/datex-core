7014c305b158b4f5ea968abed511db6545500d91
Jonas Strehle <jonas@strehles.info>
2025-08-19 17:49:07 +0200

    fix test cases

--- tests/network/execution.rs
+++ tests/network/execution.rs
@@ -1,11 +1,12 @@
 use crate::network::helpers::mock_setup::get_mock_setup_with_two_runtimes;
+use datex_core::logger::init_logger_debug;
 use datex_core::run_async;
 use datex_core::runtime::execution_context::ExecutionContext;
 use datex_core::values::core_values::endpoint::Endpoint;
 use datex_core::values::core_values::integer::integer::Integer;
+use datex_core::values::core_values::integer::typed_integer::TypedInteger;
 use datex_core::values::value_container::ValueContainer;
 use std::time::Duration;
-use datex_core::logger::init_logger_debug;
 
 #[tokio::test]
 pub async fn test_basic_remote_execution() {
@@ -23,10 +24,10 @@ pub async fn test_basic_remote_execution() {
 
         // execute script remotely on @test_b
         let result = runtime_a.execute("1 + 2", &[], Some(&mut remote_execution_context)).await;
-        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(Integer::from(3)));
+        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(TypedInteger::from(3i8)));
 
         let result = runtime_a.execute("2 + 3", &[], Some(&mut remote_execution_context)).await;
-        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(Integer::from(5)));
+        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(TypedInteger::from(5i8)));
     }
 }
 
@@ -47,11 +48,11 @@ pub async fn test_remote_execution_persistent_context() {
 
         // execute script remotely on @test_b
         let result = runtime_a.execute("const x = 10; x", &[], Some(&mut remote_execution_context)).await;
-        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(Integer::from(10)));
+        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(TypedInteger::from(10i8)));
 
         // execute another script that uses the previous context
         let result = runtime_a.execute("x + 5", &[], Some(&mut remote_execution_context)).await;
-        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(Integer::from(15)));
+        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(TypedInteger::from(15i8)));
     }
 }
 
@@ -71,7 +72,7 @@ pub async fn test_remote_inline() {
 
         // execute script remotely on @test_b
         let result = runtime_a.execute("@test_b :: 1 + 2", &[], Some(&mut execution_context)).await;
-        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(Integer::from(3)));
+        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(TypedInteger::from(3i8)));
     }
 }
 
@@ -85,9 +86,9 @@ pub async fn test_remote_inline_implicit_context() {
         // sleep for a short time to ensure the connection is established
         tokio::time::sleep(Duration::from_millis(1)).await;
         runtime_a.com_hub().print_metadata();
-        
+
         // execute script remotely on @test_b
         let result = runtime_a.execute("@test_b :: 1 + 2", &[], None).await;
-        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(Integer::from(3)));
+        assert_eq!(result.unwrap().unwrap(), ValueContainer::from(TypedInteger::from(3i8)));
     }
 }
