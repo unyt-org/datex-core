2c6b0629e7b170a20de7ba0be2c6a0d2d6a73e15
Jonas Strehle <jonas@strehles.info>
2025-08-24 11:11:45 +0200

    refactor: reorganize error module structure and enhance error handling

--- src/ast/error/error.rs
+++ src/ast/error/error.rs
@@ -1,6 +1,12 @@
-use std::ops::Range;
+use std::{io::Write, ops::Range};
 
-use crate::{ast::TokenInput, compiler::lexer::Token};
+use crate::{
+    ast::{
+        TokenInput,
+        error::{pattern::Pattern, src::SrcId},
+    },
+    compiler::lexer::Token,
+};
 
 #[derive(Debug, Clone, PartialEq)]
 
@@ -8,56 +14,197 @@ pub enum SpanOrToken {
     Span(Range<usize>),
     Token(usize),
 }
+impl From<Range<usize>> for SpanOrToken {
+    fn from(value: Range<usize>) -> Self {
+        SpanOrToken::Span(value)
+    }
+}
+impl From<usize> for SpanOrToken {
+    fn from(value: usize) -> Self {
+        SpanOrToken::Token(value)
+    }
+}
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum ParseError {
+#[derive(Debug, PartialEq, Clone)]
+pub enum ErrorKind {
+    Custom(String),
+    UnexpectedEnd,
     Unexpected {
-        span: SpanOrToken,
         found: Option<Token>,
         expected: Vec<DefaultExpected<'static, Token>>,
     },
-    UnclosedDelimiter {
-        span: SpanOrToken,
-        delimiter: Token,
-    },
-    InvalidToken {
-        span: SpanOrToken,
-        token: Token,
-    },
-    Custom {
-        span: SpanOrToken,
-        message: String,
+    Unclosed {
+        start: Pattern,
+        before_span: SpanOrToken,
+        before: Option<Pattern>,
     },
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ParseError {
+    kind: ErrorKind,
+    span: SpanOrToken,
+    while_parsing: Option<(SpanOrToken, &'static str)>,
+    label: Option<&'static str>,
+}
+
 impl ParseError {
-    pub fn new_invalid_token(token: Token, span: Range<usize>) -> Self {
-        ParseError::InvalidToken {
-            span: SpanOrToken::Span(span),
-            token,
+    pub fn new(kind: ErrorKind, span: SpanOrToken) -> Self {
+        Self {
+            kind,
+            span,
+            while_parsing: None,
+            label: None,
         }
+    }
+    pub fn new_custom(message: String, span: SpanOrToken) -> Self {
+        Self::new(ErrorKind::Custom(message), span)
+    }
+    pub fn new_unexpected_end(span: SpanOrToken) -> Self {
+        Self::new(ErrorKind::UnexpectedEnd, span)
+    }
+    pub fn new_unexpected<T: Into<SpanOrToken>>(
+        found: Option<Token>,
+        span: T,
+    ) -> Self {
+        Self::new(
+            ErrorKind::Unexpected {
+                found,
+                expected: Vec::new(),
+            },
+            span.into(),
+        )
+    }
+    pub fn new_unclosed(
+        start: Pattern,
+        before_span: SpanOrToken,
+        before: Option<Pattern>,
+    ) -> Self {
+        Self::new(
+            ErrorKind::Unclosed {
+                start,
+                before_span: before_span.clone(),
+                before,
+            },
+            before_span,
+        )
     }
 
-    fn span_or_token(&self) -> &SpanOrToken {
-        match self {
-            ParseError::Unexpected { span, .. } => span,
-            ParseError::UnclosedDelimiter { span, .. } => span,
-            ParseError::InvalidToken { span, .. } => span,
-            ParseError::Custom { span, .. } => span,
-        }
+    pub fn with_while_parsing(
+        mut self,
+        span: SpanOrToken,
+        context: &'static str,
+    ) -> Self {
+        self.while_parsing = Some((span, context));
+        self
+    }
+
+    pub fn with_label(mut self, label: &'static str) -> Self {
+        self.label = Some(label);
+        self
+    }
+}
+
+impl ParseError {
+    pub(crate) fn set_span(&mut self, span: Range<usize>) {
+        self.span = span.into();
     }
     pub fn span(&self) -> Option<&Range<usize>> {
-        match self.span_or_token() {
+        match &self.span {
             SpanOrToken::Span(span) => Some(span),
             SpanOrToken::Token(_) => None,
         }
     }
     pub fn token_pos(&self) -> Option<usize> {
-        match self.span_or_token() {
+        match &self.span {
             SpanOrToken::Span(_) => None,
             SpanOrToken::Token(pos) => Some(*pos),
         }
     }
+
+    pub fn message(&self) -> String {
+        match &self.kind {
+            ErrorKind::Custom(msg) => msg.clone(),
+            ErrorKind::UnexpectedEnd => "Unexpected end of input".to_string(),
+            ErrorKind::Unexpected { found, expected } => {
+                let mut msg = String::new();
+                if let Some(found) = found {
+                    msg.push_str(&format!("Unexpected token: {}", found));
+                } else {
+                    msg.push_str("Unexpected end of input");
+                }
+                if !expected.is_empty() {
+                    msg.push_str(", expected one of: ");
+                    let expected_strs: Vec<String> = expected
+                        .iter()
+                        .map(|e| match e {
+                            DefaultExpected::Any => "any".to_string(),
+                            DefaultExpected::Token(token) => token.as_string(),
+                            DefaultExpected::EndOfInput => {
+                                "end of input".to_string()
+                            }
+                            DefaultExpected::SomethingElse => {
+                                "something else".to_string()
+                            }
+                            e => unreachable!(
+                                "Unexpected expected variant: {:?}",
+                                e
+                            ),
+                        })
+                        .collect();
+                    msg.push_str(&expected_strs.join(", "));
+                }
+                msg
+            }
+            ErrorKind::Unclosed {
+                start,
+                before_span,
+                before,
+            } => {
+                let mut msg = format!("Unclosed delimiter: {}", start);
+                if let Some(before) = before {
+                    msg.push_str(&format!(", before {}", before));
+                }
+                msg
+            }
+        }
+    }
+
+    pub fn write<C: ariadne::Cache<SrcId>>(self, cache: C, writer: impl Write) {
+        use ariadne::{Color, Fmt, Label, Report, ReportKind};
+
+        let span = (SrcId::test(), self.span().unwrap().clone());
+
+        let report = Report::build(ReportKind::Error, span.clone())
+            .with_code("Unexpected Token")
+            .with_message(self.message())
+            .with_note("Please check the syntax and try again.")
+            .with_label(
+                Label::new(span)
+                    .with_message(match &self.kind {
+                        ErrorKind::UnexpectedEnd => "End of input".to_string(),
+                        ErrorKind::Unexpected { found, expected } => {
+                            format!(
+                                "Unexpected {}",
+                                found
+                                    .clone()
+                                    .unwrap()
+                                    .as_string()
+                                    .fg(Color::Red)
+                            )
+                        }
+                        ErrorKind::Unclosed { start, .. } => format!(
+                            "Delimiter {} is never closed",
+                            start.fg(Color::Red)
+                        ),
+                        ErrorKind::Custom(msg) => msg.clone(),
+                    })
+                    .with_color(Color::Red),
+            );
+        report.finish().write(cache, writer).unwrap();
+    }
 }
+
 use chumsky::{
     DefaultExpected,
     error::{Error, LabelError},
@@ -67,16 +214,29 @@ use chumsky::{
 
 impl<'a> Error<'a, TokenInput<'a>> for ParseError {
     fn merge(mut self, mut other: Self) -> Self {
-        if let (
-            Self::Unexpected { expected, .. },
-            Self::Unexpected {
-                expected: expected_other,
-                ..
-            },
-        ) = (&mut self, &mut other)
-        {
-            expected.append(expected_other);
-        }
+        match (&mut self.kind, &mut other.kind) {
+            (ErrorKind::Custom(msg1), ErrorKind::Custom(msg2)) => {
+                msg1.push_str(" | ");
+                msg1.push_str(msg2);
+            }
+            (ErrorKind::UnexpectedEnd, ErrorKind::UnexpectedEnd) => {}
+            (
+                ErrorKind::Unexpected {
+                    found: found1,
+                    expected: expected1,
+                },
+                ErrorKind::Unexpected {
+                    found: found2,
+                    expected: expected2,
+                },
+            ) => {
+                if found1.is_none() {
+                    *found1 = found2.take();
+                }
+                expected1.append(expected2);
+            }
+            _ => {}
+        };
         self
     }
 }
@@ -89,10 +249,25 @@ impl<'a> LabelError<'a, TokenInput<'a>, DefaultExpected<'a, Token>>
         found: Option<MaybeRef<'a, Token>>,
         span: SimpleSpan<usize>,
     ) -> Self {
-        Self::Unexpected {
+        let mut expected: Vec<DefaultExpected<'static, Token>> =
+            expected.into_iter().map(|e| e.into_owned()).collect();
+        expected.sort_by(|a, b| {
+            use DefaultExpected::*;
+            match (a, b) {
+                (SomethingElse, _) => std::cmp::Ordering::Greater,
+                (_, SomethingElse) => std::cmp::Ordering::Less,
+                _ => std::cmp::Ordering::Equal,
+            }
+        });
+        // let context = span.context();
+        ParseError {
+            kind: ErrorKind::Unexpected {
+                found: found.as_deref().cloned(),
+                expected,
+            },
             span: SpanOrToken::Token(span.start),
-            expected: expected.into_iter().map(|e| e.into_owned()).collect(),
-            found: found.as_deref().cloned(),
+            while_parsing: None,
+            label: None,
         }
     }
 }
--- src/ast/error/mod.rs
+++ src/ast/error/mod.rs
@@ -1,2 +1,3 @@
 pub mod error;
+pub mod pattern;
 pub mod src;
