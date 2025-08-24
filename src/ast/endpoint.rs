240ff99ac45aef252b874a9cba83d677ba594471
Jonas Strehle <jonas@strehles.info>
2025-08-24 14:18:08 +0200

    improve error handling for endpoints

--- src/ast/endpoint.rs
+++ src/ast/endpoint.rs
@@ -3,6 +3,7 @@ use std::str::FromStr;
 use crate::ast::DatexExpression;
 use crate::ast::DatexParserTrait;
 use crate::ast::ParserRecoverExt;
+use crate::ast::error::error::ParseError;
 use crate::compiler::lexer::Token;
 use crate::values::core_values::endpoint::Endpoint;
 use chumsky::prelude::*;
@@ -11,7 +12,9 @@ pub fn endpoint<'a>() -> impl DatexParserTrait<'a> {
     select! {
         Token::Endpoint(s) =>
             match Endpoint::from_str(s.as_str()) {
-                Err(e) => Err(e),
+                Err(e) => Err(ParseError::from(e).with_note(
+                    "Make sure the endpoint only contains valid characters."
+                )),
                 Ok(endpoint) => Ok(DatexExpression::Endpoint(endpoint))
         }
     }
