34b6a45be746c345b36ac10385ad8e25e180a60e
Jonas Strehle <jonas@strehles.info>
2025-08-25 22:22:07 +0200

    fix array and tests

--- src/ast/array.rs
+++ src/ast/array.rs
@@ -7,24 +7,30 @@ use chumsky::prelude::*;
 pub fn array<'a>(
     expression_without_tuple: impl DatexParserTrait<'a>,
 ) -> impl DatexParserTrait<'a> {
-    just(Token::LeftBracket)
-        .labelled(Pattern::Char('['))
-        .ignore_then(
-            expression_without_tuple
-                .separated_by(just(Token::Comma).padded_by(whitespace()))
-                .at_least(0)
-                .allow_trailing()
-                .collect::<Vec<_>>()
-                .labelled(Pattern::Custom("array items")),
-        )
+    expression_without_tuple
+        .clone()
+        .separated_by(just(Token::Comma).padded_by(whitespace()))
+        .at_least(0)
+        .allow_trailing()
+        .collect()
         .padded_by(whitespace())
-        .then_ignore(just(Token::RightBracket).labelled(Pattern::Char(']')))
+        .delimited_by(just(Token::LeftBracket), just(Token::RightBracket))
         .map(DatexExpression::Array)
         .labelled(Pattern::Array)
         .as_context()
-
-    // .collect::<Vec<_>>()
-    // .padded_by(whitespace())
-    // .delimited_by(just(Token::LeftBracket), just(Token::RightBracket))
-    // .map(DatexExpression::Array)
+    // just(Token::LeftBracket)
+    //     .labelled(Pattern::Char('['))
+    //     .ignore_then(
+    //         expression_without_tuple
+    //             .separated_by(just(Token::Comma).padded_by(whitespace()))
+    //             .at_least(0)
+    //             .allow_trailing()
+    //             .collect::<Vec<_>>()
+    //             .labelled(Pattern::Custom("array items")),
+    //     )
+    //     .padded_by(whitespace())
+    //     .then_ignore(just(Token::RightBracket).labelled(Pattern::Char(']')))
+    //     .map(DatexExpression::Array)
+    //     .labelled(Pattern::Array)
+    //     .as_context()
 }
