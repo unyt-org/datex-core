de6b5d4bc50ca0de98001d99f53aab334d4051e8
Jonas Strehle <jonas@strehles.info>
2025-08-28 17:58:16 +0200

    simplify literal

--- src/ast/literal.rs
+++ src/ast/literal.rs
@@ -1,6 +1,5 @@
 use crate::ast::{DatexExpression, DatexParserTrait, Slot};
 use crate::compiler::lexer::Token;
-use chumsky::extra::Err;
 use chumsky::prelude::*;
 
 pub fn literal<'a>() -> impl DatexParserTrait<'a> {
@@ -11,12 +10,6 @@ pub fn literal<'a>() -> impl DatexParserTrait<'a> {
         select! { Token::NamedSlot(s) => DatexExpression::Slot(Slot::Named(s[1..].to_string())) },
         select! { Token::Slot(s) => DatexExpression::Slot(Slot::Addressed(s[1..].parse::<u32>().unwrap())) },
         select! { Token::Placeholder => DatexExpression::Placeholder },
-        select! { Token::Identifier(name) => name }
-            .then(
-                just(Token::Slash)
-                    .ignore_then(select! { Token::Identifier(sub) => sub })
-                    .or_not(),
-            )
-            .map(|(name, variant)| DatexExpression::Literal { name, variant }),
+        select! { Token::Identifier(name) => DatexExpression::Literal(name) },
     ))
 }
