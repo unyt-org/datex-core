b70129b227680a5c04934ff9b32bc651dbdefbd0
Jonas Strehle <jonas@strehles.info>
2025-09-06 11:56:12 +0200

    Add test for media track creation and ICE candidate handling in WebRTCNativeInterface  (WIP)

--- tests/network/com_interfaces/webrtc_native.rs
+++ tests/network/com_interfaces/webrtc_native.rs
@@ -5,7 +5,10 @@ use datex_core::{
         com_interface::ComInterface,
         com_interface_socket::ComInterfaceSocketUUID,
         default_com_interfaces::webrtc::{
-            webrtc_common::webrtc_trait::{WebRTCTrait, WebRTCTraitInternal},
+            webrtc_common::{
+                media_tracks::MediaKind,
+                webrtc_trait::{WebRTCTrait, WebRTCTraitInternal},
+            },
             webrtc_native_interface::WebRTCNativeInterface,
         },
         socket_provider::SingleSocketProvider,
@@ -117,5 +120,62 @@ pub async fn test_connect() {
         // Check if the messages are received correctly
         assert_eq!(receive_a, BLOCK_B_TO_A);
         assert_eq!(receive_b, BLOCK_A_TO_B);
+    }
+}
+
+#[tokio::test]
+#[timeout(10000)]
+pub async fn test_media_track() {
+    run_async! {
+        init_global_context();
+        // Create a WebRTCNativeInterface instance on each side (remote: @a)
+        let mut interface_a = WebRTCNativeInterface::new(
+            TEST_ENDPOINT_A.clone(),
+        );
+        interface_a.open().await.unwrap();
+
+
+        // Create a WebRTCNativeInterface instance on each side (remote: @b)
+        let mut interface_b = WebRTCNativeInterface::new(
+            TEST_ENDPOINT_B.clone(),
+        );
+        interface_b.open().await.unwrap();
+
+        let interface_a = Rc::new(RefCell::new(interface_a));
+        let interface_b = Rc::new(RefCell::new(interface_b));
+
+        let interface_a_clone = interface_a.clone();
+        let inteface_b_clone = interface_b.clone();
+
+        interface_a.clone().borrow().set_on_ice_candidate(Box::new(move |candidate| {
+            let interface_b = inteface_b_clone.clone();
+            spawn_local(async move {
+                interface_b.clone().borrow().add_ice_candidate(candidate).await.unwrap();
+            });
+        }));
+
+        interface_b.clone().borrow().set_on_ice_candidate(Box::new(move |candidate| {
+            let interface_a = interface_a_clone.clone();
+            spawn_local(async move {
+                interface_a.clone().borrow().add_ice_candidate(candidate).await.unwrap();
+            });
+        }));
+
+
+        let offer = interface_a.clone().borrow().create_offer().await.unwrap();
+
+        let answer = interface_b.clone().borrow().create_answer(offer).await.unwrap();
+        interface_a.clone().borrow().set_answer(answer).await.unwrap();
+
+        interface_a.borrow().wait_for_connection().await.unwrap();
+        interface_b.borrow().wait_for_connection().await.unwrap();
+
+        interface_a.borrow().create_media_track(MediaKind::Video).await.unwrap();
+
+        // Wait for the messages to be received
+        sleep(Duration::from_secs(1)).await;
+
+        println!("Tracks A: {:?}", interface_a.borrow().provide_media_channels().borrow().tracks.values().next().unwrap().borrow().kind);
+        // println!("Tracks B: {:?}", interface_b.borrow().provide_media_channels().borrow().tracks.values().next().unwrap().borrow().kind);
     }
 }
