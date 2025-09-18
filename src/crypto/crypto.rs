b3b5862b13cd44595598abbc23dfd94c9c216cba
janiejestemja <janiejestemja@gmail.com>
2025-09-18 09:07:48 +0200

    removed rsa signatures

--- src/crypto/crypto.rs
+++ src/crypto/crypto.rs
@@ -13,26 +13,10 @@ pub trait CryptoTrait: Send + Sync {
         private_key: Vec<u8>,
     ) -> Pin<Box<dyn Future<Output = Result<Vec<u8>, CryptoError>>>>;
 
-    fn sign_rsa(
-        &self,
-        data: Vec<u8>,
-        private_key: Vec<u8>,
-    ) -> Pin<Box<dyn Future<Output = Result<Vec<u8>, CryptoError>>>>;
-
-    fn verify_rsa(
-        &self,
-        data: Vec<u8>,
-        signature: Vec<u8>,
-        public_key: Vec<u8>,
-    ) -> Pin<Box<dyn Future<Output = Result<bool, CryptoError>>>>;
-
     fn create_uuid(&self) -> String;
     fn random_bytes(&self, length: usize) -> Vec<u8>;
 
     fn new_encryption_key_pair(
-        &self,
-    ) -> Pin<Box<dyn Future<Output = Result<(Vec<u8>, Vec<u8>), CryptoError>>>>;
-    fn new_sign_key_pair(
         &self,
     ) -> Pin<Box<dyn Future<Output = Result<(Vec<u8>, Vec<u8>), CryptoError>>>>;
 
