5dd42c4d6e21409256e47d2785db09ffa972f7dc
Benedikt Strehle <benedikt@unyt.org>
2025-10-19 17:47:59 +0800

    :recycle: type inference refactoring (wip)

--- src/compiler/precompiler.rs
+++ src/compiler/precompiler.rs
@@ -1,6 +1,6 @@
 use crate::ast::binary_operation::{ArithmeticOperator, BinaryOperator};
 use crate::ast::chain::ApplyOperation;
-use crate::compiler::error::{CompilerError, DetailedCompilerErrors, SimpleOrDetailedCompilerError, SpannedCompilerError};
+use crate::compiler::error::{collect_or_pass_error, CompilerError, DetailedCompilerErrors, ErrorCollector, MaybeAction, SimpleOrDetailedCompilerError, SpannedCompilerError};
 use crate::libs::core::CoreLibPointerId;
 use crate::references::type_reference::{
     NominalTypeDeclaration, TypeReference,
@@ -344,39 +344,6 @@ enum NewScopeType {
     NewScope,
     // create a new scope and increment the realm index (e.g. for remote execution calls)
     NewScopeWithNewRealm,
-}
-
-/// Describes an optional action that is only executed if an Ok result
-/// was returned (used in collect_or_pass_error);
-enum MaybeAction<T> {
-    // optional action should not be performed
-    Skip,
-    // action should be performed with the provided value
-    Do(T)
-}
-
-/// Handles a generic Result with an SpannedCompilerError error.
-/// If the result is Ok(), an Ok(MaybeAction::Do) with the result is returned
-/// If result is Error() and collected_errors is Some, the error is appended to the collected_errors
-/// and an Ok(MaybeAction::Skip) is returned
-/// If result is Error() and collected_errors is None, the error is directly returned
-fn collect_or_pass_error<T>(
-    collected_errors: &mut Option<DetailedCompilerErrors>,
-    result: Result<T, SpannedCompilerError>,
-) -> Result<MaybeAction<T>, SpannedCompilerError> {
-    if let Ok(result) = result {
-        Ok(MaybeAction::Do(result))
-    }
-    else {
-        let error = unsafe { result.unwrap_err_unchecked() };
-        if let Some(collected_errors) = collected_errors {
-            collected_errors.record_error(error);
-            Ok(MaybeAction::Skip)
-        }
-        else {
-            Err(error)
-        }
-    }
 }
 
 fn visit_expression(
