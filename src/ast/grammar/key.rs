b3dce6bd95446cade498a59b738c7b988db0cc97
Jonas Strehle <jonas@strehles.info>
2025-11-15 14:31:10 +0100

    work on integer grammar (WIP)

--- src/ast/grammar/key.rs
+++ src/ast/grammar/key.rs
@@ -1,7 +1,8 @@
 use crate::ast::grammar::text::text;
-use crate::ast::lexer::Token;
+use crate::ast::lexer::{IntegerLiteral, Token};
 use crate::ast::spanned::Spanned;
-use crate::ast::{DatexExpressionData, DatexParserTrait};
+use crate::ast::{DatexExpressionData, DatexParserTrait, ParserRecoverExt};
+use crate::values::core_values::integer::Integer;
 use chumsky::prelude::*;
 /// A valid map key
 /// abc, a, "1", "test", (1 + 2), ...
@@ -12,9 +13,14 @@ pub fn key<'a>(
         text(),
         // any valid identifiers (equivalent to variable names), mapped to a text
         select! {
-            Token::Identifier(s) => DatexExpressionData::Text(s)
+             Token::Identifier(s) => DatexExpressionData::Text(s),
+        }.map_with(|data, e| data.with_span(e.span())),
+        select! {
+            Token::DecimalIntegerLiteral(IntegerLiteral { value, variant: None }) =>
+                Integer::from_string(&value).map(DatexExpressionData::Integer),
         }
-        .map_with(|data, e| data.with_span(e.span())),
+        .map_with(|data, e| data.map(|data| data.with_span(e.span())))
+        .recover_invalid(),
         // dynamic key
         wrapped_expression.clone(),
     ))
