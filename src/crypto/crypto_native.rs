ee7a0c85c16feb72e9c0fd6b08724bc6464860bd
janiejestemja <janiejestemja@web.de>
2025-12-15 20:19:28 +0100

    add base58 fingerprints

--- src/crypto/crypto_native.rs
+++ src/crypto/crypto_native.rs
@@ -293,6 +293,16 @@ mod tests {
         );
     }
 
+    #[test]
+    pub fn base58() {
+        let something = b"Something".to_vec();
+        let based = CRYPTO.enc_b58(&something).unwrap();
+        let unbased = CRYPTO.dec_b58(&based).unwrap();
+        assert_eq!(something, unbased);
+        println!("Based: {:?}", based);
+        println!("Unbased: {:?}", unbased);
+    }
+
     // Signatures
     #[tokio::test]
     pub async fn dsa_ed25519() {
