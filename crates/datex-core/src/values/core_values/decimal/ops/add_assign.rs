c83e695dbd541f154004aa3599fef3f1b5430e06
Jonas Strehle <jonas@strehles.info>
2026-09-08 09:59:48 +0200

    wip ops fixes

--- crates/datex-core/src/values/core_values/decimal/ops/add_assign.rs
+++ crates/datex-core/src/values/core_values/decimal/ops/add_assign.rs
@@ -1,24 +1,24 @@
 use crate::values::core_values::decimal::{
     Decimal, typed_decimal::TypedDecimal,
 };
-use core::ops::{Add, AddAssign};
+use core::ops::AddAssign;
 impl AddAssign for TypedDecimal {
     fn add_assign(&mut self, rhs: Self) {
-        *self = TypedDecimal::add(self.clone(), rhs);
+        *self = self as &TypedDecimal + &rhs;
     }
 }
 impl AddAssign for Decimal {
     fn add_assign(&mut self, rhs: Self) {
-        *self = Decimal::add(self.clone(), rhs);
+        *self = self as &Decimal + &rhs;
     }
 }
 impl AddAssign<&Decimal> for Decimal {
     fn add_assign(&mut self, rhs: &Decimal) {
-        *self = Decimal::add(self.clone(), rhs.clone());
+        *self = self as &Decimal + rhs;
     }
 }
 impl AddAssign<&TypedDecimal> for TypedDecimal {
     fn add_assign(&mut self, rhs: &TypedDecimal) {
-        *self = TypedDecimal::add(self.clone(), rhs.clone());
+        *self = self as &TypedDecimal + rhs;
     }
 }
--- crates/datex-core/src/values/core_values/integer/ops/add_assign.rs
+++ crates/datex-core/src/values/core_values/integer/ops/add_assign.rs
@@ -1,10 +1,9 @@
 use core::ops::AddAssign;
 
 use crate::values::core_values::integer::typed_integer::TypedInteger;
-use core::ops::Add;
 impl AddAssign for TypedInteger {
     // FIXME #345 error handling / wrapping if out of bounds
     fn add_assign(&mut self, rhs: Self) {
-        *self = TypedInteger::add(self.clone(), rhs).expect("Failed to add");
+        *self = (self as &TypedInteger + &rhs).expect("Failed to add");
     }
 }
